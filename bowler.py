9eae606db14488506cd7607d37db854cbf37b659
Matt <mattpaulrose@gmail.com>
2014-10-24 13:02:45 -0500

    All that's left on the logic side is writing the damn strike scoring stuff.

--- bowler.py
+++ bowler.py
@@ -14,30 +14,22 @@ class Bowler(object):
 
     def bowl(self, pins_left):
 
-        if not pins_left:
-            if not len(self.get_current_frame()):
-                print "Strike!"
-                pin_count = 10
-            else:
-                print "Spare!"
-                pin_count = 10 - self.get_current_frame()[0]
-            self.frames[self.current_frame].append(pin_count - pins_left)
-            self.advance_frame()
+        if not len(self.get_current_frame()):
+            pin_count = 10
         else:
-            if not len(self.get_current_frame()):
-                pin_count = 10
-            else:
-                pin_count = 10 - self.get_current_frame()[0]
-            self.frames[self.current_frame].append(pin_count - pins_left)
-            if len(self.frames[self.current_frame]) == 2:
-                self.advance_frame()
+            pin_count = 10 - self.get_current_frame()[0]
+        self.frames[self.current_frame].append(pin_count - pins_left)
+        frame = self.frames[self.current_frame]
+        if len(frame) == 2 or sum(frame) == 10:
+            self.advance_frame()
 
         print 'Current frame: ' + str(self.current_frame)
         print 'Frame scores: ' + str(self.frames)
         print 'Score: ' + str(self.score)
 
     def advance_frame(self):
-        self.check_strike_or_spare()
+        if self.score:
+            self.check_strike_or_spare()
         if len(self.frames):
             self.score.append(sum(self.get_current_frame()))
         self.current_frame += 1
@@ -45,17 +37,15 @@ class Bowler(object):
 
     def check_strike_or_spare(self):
         for frame, frame_score in self.frames.iteritems():
-            print frame
-            if sum(frame_score) == 10 and self.score and self.score[frame] <= 10:
-                if len(frame_score) == 1 and len(self.frames) - 2 > frame:
+            if sum(frame_score) == 10 and len(self.score) > frame and self.score[frame] <= 10:
+                if len(frame_score) == 1 and len(self.score) > 2:
                     # Strike
-                    self.score[frame] += sum(self.frames[frame + 1])
-                    if len(self.frames[frame + 1]) == 1:
-                        self.score[frame] += self.frames[frame + 2][0]
-                else:
+                    print 'Strike!'
+                    
+                elif len(frame_score) != 1:
                     # Spare
+                    print 'Spare!'
                     self.score[frame] += self.frames[frame + 1][0]
-
 
     def get_current_frame(self):
         return self.frames[self.current_frame]
@@ -67,9 +57,14 @@ class Bowler(object):
 
 
 bob = Bowler("Bob")
-bob.bowl(5)
 bob.bowl(0)
-bob.bowl(1)
 bob.bowl(0)
-bob.bowl(5)
-bob.bowl(4)
+bob.bowl(0)
+bob.bowl(0)
+bob.bowl(0)
+bob.bowl(0)
+bob.bowl(0)
+bob.bowl(0)
+bob.bowl(0)
+bob.bowl(0)
+bob.bowl(0)
